c411d70ce181713bc652d67760c87b235c7e04d8
Broveku <gearhearjordan@gmail.com>
2021-09-06 16:00:44 -0700

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,6 +1,21 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
-  
-  next()
+  const error = { status: 400 }
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined){
+    error.message = 'name and budget are required'
+    next(error)
+  } else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+    next(error)
+  } else if (name.trim().length < || name.trim().length > 100) {
+    error.message = 'name of account must be between 3 and 100'
+    next(error)
+  } else if (typeof budget !== 'number' || !isNaN(budget)) {
+    error.message = 'budget of account must be a number'
+    next(error)
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
@@ -8,7 +23,17 @@ exports.checkAccountNameUnique = (req, res, next) => {
   next()
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
+  try{
+    const account = await Account.getById(req.params.id)
+    if (!account){
+      next({ status: 404, message: 'account not found'})
+    } else {
+      req.account = account
+      next()
+    }
+  }catch (err) {
+    next(err)
+  }
   
-  next()
 }
