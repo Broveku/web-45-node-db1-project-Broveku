ea56f34f88fea89be56db65705e1e870dcd1ee6e
Broveku <gearhearjordan@gmail.com>
2021-09-06 15:28:33 -0700

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,20 +1,21 @@
 const router = require('express').Router()
 const md = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
 
-router.get('/', (req, res, next) => {
-  
+router.get('/', async (req, res, next) => {
   try{
-      res.json({})
+    const accounts = await Account.getAll()
+    res.json(accounts)
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:id', md.checkAccountId, (req, res, next) => {
-  
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
   try{
-
+    const account = await Account.getById(req.params.id)
+    res.json(account)
   } catch (err){
     next(err)
   }
@@ -33,7 +34,7 @@ router.post('/',
 router.put('/:id',
 md.checkAccountId,
 md.checkAccountPayload,
-md.checkAccountNameUnique  (req, res, next) => {
+md.checkAccountNameUnique,  (req, res, next) => {
   try{
 
   } catch (err) {
